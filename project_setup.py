d5b47b0db87720121c56b786df49322e53697eff
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-27 23:57:46 -0300

    Criando arquivo para guardar constantes

--- project_setup.py
+++ project_setup.py
@@ -1,16 +1,5 @@
-from pathlib import Path
+from constants import *
 from db_tools import db_connection
-
-# Construindo o caminho completo para o diretório base do projeto
-BASE_DIR = Path(__file__).resolve().parent
-
-# Construindo o caminho completo para o resto da estrutura do projeto
-BACKUPS_DIR = BASE_DIR / "backups"
-DATA_DIR = BASE_DIR / "data"
-EXPORTS_DIR = BASE_DIR / "exports"
-IMPORTS_DIR = BASE_DIR / "imports"
-
-LIBRARY_DB = BASE_DIR / DATA_DIR / "library.db"
 
 
 @db_connection
@@ -20,23 +9,11 @@ def mk_table(cursor):
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             author TEXT NOT NULL,
-            publication_year DATE NOT NULL,
-            price REAL NOT NULL   
+            price REAL NOT NULL,   
+            pub_year TEXT NOT NULL
         )
     ''')
 
-
-""" DESCOMENTE PARA TESTAR AS INSERÇÕES
-    cursor.execute('''
-       INSERT INTO books(author, title, publication_year, price)
-       VALUES ('Diário de um banana 1', 'Jeff Kinney', '2008-05-19', 46)
-       ''')
-
-    cursor.execute('''
-        INSERT INTO books(author, title, publication_year, price)
-        VALUES ('O Menino Maluquinho', 'Ziraldo', '2023-12-20', 25.20)
-    ''')
-"""
 
 # Definindo a função encarregada de criar toda a estrutura do projeto caso já não exista
 def project_setup():
