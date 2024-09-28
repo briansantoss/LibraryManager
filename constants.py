eb89254b54b5b44d3a14408fdf428b8b8766aaea
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-28 01:41:24 -0300

    Implementando menus e laços de repetição

--- constants.py
+++ constants.py
@@ -11,3 +11,26 @@ IMPORTS_DIR = BASE_DIR / "imports"
 
 # Construindo o caminho completo para o arquivo do banco de dados
 LIBRARY_DB = BASE_DIR / DATA_DIR / "library.db"
+
+# Definindo as opções/strings de cada um dos menus
+MAIN_MENU = '''
+[1] - Library Options
+[2] - Admin Options
+[3] - Quit System
+'''
+
+LIBRARY_MENU = '''
+[1] - Add book
+[2] - Display book(s) information(s)
+[3] - Modify book price
+[4] - Remove book
+[5] - Filter book(s) by author
+[6] - Go back
+'''
+
+ADMIN_MENU = '''
+[1] - Export CSV data
+[2] - Import CSV data
+[3] - System Backup
+[4] - Go back
+'''
